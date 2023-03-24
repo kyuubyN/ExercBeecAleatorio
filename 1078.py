83e46ef64bca88fd262abaf47e6ce9a905000e40
Kaue - kyuubyN <102341451+kyuubyN@users.noreply.github.com>
2023-03-24 01:32:12 -0300

    Create 1078.py

---
+++ 1078.py
@@ -0,0 +1,9 @@
+n = int(input())
+x = 0
+y = 0
+while x != 10:
+    x += 1
+    y += 1
+    z = n * y
+    print(f"{y} x {n} = {z}")
+#Este codigo pega qualquer numero e mostra a tabuada dele até o 10
