fd27da16de6e65eddf3117e9cc89c7065b753f65
Kaue - kyuubyN <102341451+kyuubyN@users.noreply.github.com>
2023-03-24 01:40:33 -0300

    Create 1114.py

---
+++ 1114.py
@@ -0,0 +1,7 @@
+senha = 2002
+teste = int(input())
+while teste != senha:
+    print("Senha invalida")
+    teste = int(input())
+print("Acesso permitido")
+#Enquanto o input não for igual a senha, vai dar Senha invalida. E caso o usuario acerte, vai dar acesso permitido
